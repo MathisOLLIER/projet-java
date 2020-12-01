81c7c5ae81793d465de03fdbace280cf1829be02
Mathis OLLIER <maol.devpro@gmail.com>
2020-12-01 10:04:05 +0100

    exo 3 finis

--- src/main/java/com/myaudiolibrary/web/Controller/ArtistController.java
+++ src/main/java/com/myaudiolibrary/web/Controller/ArtistController.java
@@ -2,10 +2,14 @@ package com.myaudiolibrary.web.Controller;
 
 import com.myaudiolibrary.web.Repository.ArtistsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import com.myaudiolibrary.web.Model.Artist;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -19,5 +23,25 @@ public class ArtistController {
             @PathVariable(value = "id") Integer id){
         Optional<Artist> artist = artistsRepository.findById(id);
         return artist.get();
+
     }
+
+    @RequestMapping(method = RequestMethod.GET, value= "", params = {"name"}, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Artist searchArtistByName(@RequestParam String name){
+        System.out.println(name);
+        Artist searchedArtist = artistsRepository.findByName(name);
+        System.out.println(searchedArtist);
+        return searchedArtist;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Page<Artist> listArtist(
+            @RequestParam Integer page,
+            @RequestParam Integer size,
+            @RequestParam String sortProperty,
+            @RequestParam String sortDirection
+    ){
+        return artistsRepository.findAll(PageRequest.of(page, size, Sort.Direction.fromString(sortDirection), sortProperty));
+    }
+
 }
--- src/main/java/com/myaudiolibrary/web/Repository/ArtistsRepository.java
+++ src/main/java/com/myaudiolibrary/web/Repository/ArtistsRepository.java
@@ -3,6 +3,10 @@ package com.myaudiolibrary.web.Repository;
 import com.myaudiolibrary.web.Model.Artist;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ArtistsRepository extends JpaRepository<Artist, Integer> {
+import java.util.List;
 
+public interface ArtistsRepository extends JpaRepository<Artist, Integer> {
+    Artist findByName(String name);
+
+    List<Artist> findByNameAndId(String name, Integer id);
 }
